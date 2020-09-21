8b125f8c31b25ec80a4202ee6999f44ee4fefe59
unknown <celine007@gmail.com>
2020-09-21 10:31:18 -0700

    Updates

---
+++ precache-manifest.a2ef74e41ff640165f96ee126ff0f120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e147b9d4bbe4f1c4d2faa173487bf6",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "bd17c2330fe18c747972",
+    "url": "/calculator/static/css/main.fa8773bc.chunk.css"
+  },
+  {
+    "revision": "47a02e533bcc8fc9a740",
+    "url": "/calculator/static/js/2.b0efd443.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.b0efd443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd17c2330fe18c747972",
+    "url": "/calculator/static/js/main.7cfdedaf.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
